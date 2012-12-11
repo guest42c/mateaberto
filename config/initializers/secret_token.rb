be507bedfa45f6c98f20bfbb4b75b1198534cf7b
Guilherme Araujo <gui2811@gmail.com>
2012-12-11 14:12:22 -0200

    create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mateaberto::Application.config.secret_token = '54c77d2460a0a81f853f2feed64bf01a0d0728e475b59b0b667a11d990aac23560858728bb95ed81d353accfda813b91d42e96e9a2a1d8eabe55516bafe615f2'
