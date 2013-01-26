6f5b1d19d93ac8594c4e1afcc6ccf207aefe6805
Guilherme Araujo <gui2811@gmail.com>
2013-01-26 17:44:25 -0200

    fos logo

--- app/controllers/installfos_controller.rb
+++ app/controllers/installfos_controller.rb
@@ -1,6 +1,7 @@
 class InstallfosController < ApplicationController
 
   def get
+    @headers["Content-Type"] = "application/x-web-app-manifest+json"
     send_file params[:filename], :type => 'application/x-web-app-manifest+json'
   end
 
