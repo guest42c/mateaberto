afe8e24da003f971a180f2635ae5c85ca8f9775a
Guilherme Araujo <gui2811@gmail.com>
2013-01-26 18:55:31 -0200

    right content-type

--- app/controllers/installfos_controller.rb
+++ app/controllers/installfos_controller.rb
@@ -1,8 +1,11 @@
 class InstallfosController < ApplicationController
 
   def get
-    @headers["Content-Type"] = "application/x-web-app-manifest+json"
-    send_file params[:filename], :type => 'application/x-web-app-manifest+json'
+    #send_file '/manifest.webapp', :type => "application/x-web-app-manifest+json"
+    #render :file => "manifest.webapp", 
+    #  :content_type => 'text/json', :layout => false
+    render :file => File.join(Rails.root, 'public', 'manifest.webapp'), 
+      :status => 200, :layout => false, :content_type => "application/x-web-app-manifest+json"
   end
 
 end
