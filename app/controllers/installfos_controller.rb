471b1c52c13a3666903e4c31b3e05b4c8a30776b
Guilherme Araujo <gui2811@gmail.com>
2013-01-27 15:26:18 -0200

    sera que agora vai

--- app/controllers/installfos_controller.rb
+++ app/controllers/installfos_controller.rb
@@ -1,9 +1,12 @@
 class InstallfosController < ApplicationController
+  before_filter :load_manifest
 
-  @data = File.read("#{Rails.root}/public/installfos")
+  def load_manifest
+    @data = File.read("#{Rails.public_path}/installfos")
+  end  
+  
   def get
-    #render :file => File.join(Rails.root, 'public', 'manifest.webapp'), 
-    #  :status => 200, :layout => false, :content_type => "application/x-web-app-manifest+json"
+    #render :json => @data
     render :json => @data, :content_type => "application/x-web-app-manifest+json", :layout => false  
   end
 
